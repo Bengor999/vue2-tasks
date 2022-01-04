6dda6697084b6e585cff35c8acbb962d3c1a9d74
artem <artjombebenin@gmail.com>
2022-01-04 23:45:38 +0300

    task 004 is ready!

--- components/app.vue
+++ components/app.vue
@@ -1,14 +1,42 @@
 <template>
-  <div> {{text}} </div>
+  <div>
+    <b> default value for all: </b> <input type="text" v-model="defaultValue"/>
+    <hr/>
+    <my-circle
+      :defaultValue="defaultValue"
+      :isRandomize="isRandomizeDefaultValue"></my-circle>
+    <!-- <my-circle
+      :defaultValue="defaultValue"
+      :isRandomize="isRandomizeDefaultValue"></my-circle> -->
+    <br/>
+    <button @click="addCircle">
+      add another a circle component
+    </button>
+    <label>
+      <input type="checkbox" v-model="isRandomizeDefaultValue" />
+      randomize default value for circle
+    </label>
+  </div>
 </template>
 
 <script>
+  const myCircle = httpVueLoader('./my-circle.vue')
   module.exports = {
     name: 'app-vue',
     data () {
       return {
-        text: 'i am app-vue'
+        defaultValue: '10',
+        isRandomizeDefaultValue: false,
+        circleCounter: 1
       }
+    },
+    methods: {
+      addCircle () {
+        this.circleCounter++
+      }
+    },
+    components: {
+      myCircle
     }
   }
 </script>
---
+++ components/my-circle.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input v-model="customValue"/>
+    <svg width="200" height="10">
+      <rect x="0" y="0" width="100%" height="100%"/>
+      <circle cx="100" :r="circleRadius" cy="5" fill="yellow" />
+    </svg>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  name: 'my-circle',
+  props: {
+    defaultValue: '0',
+    isRandomize: false
+  },
+  data() {
+    return {
+      customValue: null,
+    }
+  },
+  computed: {
+    circleRadius () {
+      return '0'
+    }
+  }
+}
+</script>
